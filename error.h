4a365422f50b6e7e9c8a928af1f1fff3b702e382
pxmpkeen <pxmpkeen@gmail.com>
2023-12-29 07:59:34 -0800

    implementation of strtok

---
+++ error.h
@@ -0,0 +1,10 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+#define PROC_CREAT_ERR	"Can't create a process"
+#define PROC_WAIT_ERR	"Can't wait for termination of child process"
+#define PROC_EXEC_ERR	"Can't execute a process"
+#define STD_READ_ERR	"Error reading standard input"
+#define ALLOC_ERR	"Can't allocate memory"
+#define PS_ERR		"Error outputting prompt string"
+#endif /* ERROR_H */
